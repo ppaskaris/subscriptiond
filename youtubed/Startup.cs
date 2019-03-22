32c5c79533f50386abb853031184b7a456879f16
Patrick Paskaris <patrick@paskaris.gr>
2019-03-22 07:03:44 -0400

    Upgrade to netcore2.2

--- youtubed/Startup.cs
+++ youtubed/Startup.cs
@@ -50,7 +50,7 @@ namespace youtubed
 
         // This method gets called by the runtime. Use this method to configure
         // the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, Microsoft.AspNetCore.Hosting.IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
